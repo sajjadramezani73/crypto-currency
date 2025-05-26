d1dc5a64209604ee4562e4477bd8ba0077f2e02a
sajjadramezani <62692232+sajjadramezani73@users.noreply.github.com>
2025-05-26 22:32:55 +0330

    create shadcn-ui config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="default" size="lg">
+        کلیک کنید
+      </Button>
+    </>
+  );
+}
+
+export default App;
