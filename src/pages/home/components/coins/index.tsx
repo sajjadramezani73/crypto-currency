ec063c9c66dc5a81379203c8f3096083b29e3028
sajjadramezani <62692232+sajjadramezani73@users.noreply.github.com>
2025-06-07 18:04:18 +0330

    add table for show coins - not complete

--- src/pages/home/components/coins/index.tsx
+++ src/pages/home/components/coins/index.tsx
@@ -3,8 +3,12 @@ import { useEffect, useRef, useState } from "react";
 
 import Card from "@/components/global/card";
 
+import { Table } from "@/components/global/table";
+
 import { getCoins } from "@/requests/api/coin";
 import { QUERY_KEYS } from "@/requests/queryKeys";
+
+import formatNumber from "@/utils/formatNumber";
 
 const Coins = () => {
   const latestPricesRef = useRef<Record<string, number>>({});
@@ -38,21 +42,82 @@ const Coins = () => {
 
   console.log({ isLoading });
 
+  const columns = [
+    {
+      header: "Coin Name",
+      className: "w-[20%]",
+      cell: (item: ICoinProps) => (
+        <div className="flex items-center gap-x-2">
+          <img src={item.image} width={20} alt={item.name} />
+          <p className="text-sm text-titr font-medium">
+            {item.name} ({item.symbol.toUpperCase()})
+          </p>
+        </div>
+      ),
+      key: "coinInfo",
+    },
+    {
+      header: "Coin Price",
+      className: "w-[20%]",
+      cell: (item: ICoinProps) => (
+        <div className="flex items-center gap-x-1">
+          $
+          <p className="text-sm text-titrDark">
+            {formatNumber(coins[item.id] ?? item.current_price, 0.01)}
+          </p>
+        </div>
+      ),
+      key: "coinPrice",
+    },
+    {
+      header: "24%",
+      className: "w-[20%]",
+      cell: (item: ICoinProps) =>
+        item.price_change_percentage_24h >= 0 ? (
+          <p className="text-sm text-green-500">
+            +{item.price_change_percentage_24h.toFixed(2)}% ⬈
+          </p>
+        ) : (
+          <p className="text-sm text-red-500">
+            {item.price_change_percentage_24h.toFixed(2)}% ⬊
+          </p>
+        ),
+      key: "pricePercentage",
+    },
+    {
+      header: "24H High",
+      className: "w-[20%]",
+      cell: (item: ICoinProps) => (
+        <div className="flex items-center gap-x-1">
+          $
+          <p className="text-sm text-titrDark">
+            {formatNumber(item.high_24h, 0.01)}
+          </p>
+        </div>
+      ),
+      key: "high24h",
+    },
+    {
+      header: "24H Low",
+      className: "w-[20%]",
+      cell: (item: ICoinProps) => (
+        <div className="flex items-center gap-x-1">
+          $
+          <p className="text-sm text-titrDark">
+            {formatNumber(item.low_24h, 0.01)}
+          </p>
+        </div>
+      ),
+      key: "low24h",
+    },
+  ];
+
+  console.log({ coins });
+  console.log(data?.data);
+
   return (
     <Card title="Coin list">
-      {data?.data?.map((coin) => {
-        const livePrice = coins[coin.id];
-        return (
-          <tr key={coin.id}>
-            <td>
-              <img src={coin.image} width={24} /> {coin.name} (
-              {coin.symbol.toUpperCase()})
-            </td>
-            <td>${livePrice || coin.current_price}</td>
-            <td>{/* درصد تغییر یا چارت کوچیک */}</td>
-          </tr>
-        );
-      })}
+      <Table<ICoinProps> items={data?.data ?? []} columns={columns} />
     </Card>
   );
 };
